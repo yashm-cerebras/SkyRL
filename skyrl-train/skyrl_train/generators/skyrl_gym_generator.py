c9827b3a633b197556426d13285583f8e31f73e0
Sumanth R Hegde <39546518+SumanthRH@users.noreply.github.com>
2025-08-15 11:24:57 -0700

    [Fix] revert "Optimize tuple unpacking in skyrl_gym_generator.generate() " (#150)
    # What does this PR do?
    
    
    Reverts #138 since it's incorrect after #145 - `rollout_logprobs` is
    also returned now and it's dtype is `Optional[List[float]]`.
    
    Also simplifies original approach a bit
    
    ---------
    
    Signed-off-by: SumanthRH <sumanthrh99@gmail.com>

--- skyrl-train/skyrl_train/generators/skyrl_gym_generator.py
+++ skyrl-train/skyrl_train/generators/skyrl_gym_generator.py
@@ -306,10 +306,11 @@ class SkyRLGymGenerator(GeneratorInterface):
             mininterval=5,
         )
 
-        if not all_outputs:
-            responses, rewards, stop_reasons, loss_masks, prompt_token_ids = [], [], [], [], []
-        else:
-            responses, rewards, stop_reasons, loss_masks, prompt_token_ids = map(list, zip(*all_outputs))
+        responses = [output[0] for output in all_outputs]
+        rewards = [output[1] for output in all_outputs]
+        stop_reasons = [output[2] for output in all_outputs]
+        loss_masks = [output[3] for output in all_outputs]
+        prompt_token_ids = [output[4] for output in all_outputs]
 
         if sampling_params is not None:
             # sampling params will be a dict in the format of the inference engine backend
@@ -319,7 +320,7 @@ class SkyRLGymGenerator(GeneratorInterface):
             get_logprobs = self.generator_cfg.sampling_params.logprobs is not None
 
         if get_logprobs:
-            rollout_logprobs = sum([[output[5]] for output in all_outputs], [])
+            rollout_logprobs = [output[5] for output in all_outputs]
         else:
             rollout_logprobs = None
 
