1b1f3348c5e98477e6513b4fbbae28d6352bd79b
Sumanth R Hegde <39546518+SumanthRH@users.noreply.github.com>
2025-09-26 16:24:19 -0700

    [Metrics] Ignore observation tokens while calculating rollout train logprobs difference  (#354)
    As title. We need to exclude the observation tokens because the rollout
    logprobs for these tokens will be 0 (dummy) and thus the overall
    logprobs difference will be artificially inflated.
    
    ---------
    
    Signed-off-by: SumanthRH <sumanthrh99@gmail.com>

--- skyrl-train/skyrl_train/trainer.py
+++ skyrl-train/skyrl_train/trainer.py
@@ -890,7 +890,12 @@ class RayPPOTrainer:
 
         if self.cfg.generator.sampling_params.logprobs is not None:
             # calculates the difference in probs between inference and trainer components
-            prob_diff = (training_input["rollout_logprobs"].exp() - action_log_probs.exp()).abs()
+            # only consider response tokens
+            logprobs_diff = (
+                training_input["rollout_logprobs"][training_input["loss_mask"] > 0]
+                - action_log_probs[training_input["loss_mask"] > 0]
+            )
+            prob_diff = logprobs_diff.exp().abs()
             prob_diff_mean = prob_diff.mean().item()
             prob_diff_std = prob_diff.std().item()
             self.all_metrics.update(
