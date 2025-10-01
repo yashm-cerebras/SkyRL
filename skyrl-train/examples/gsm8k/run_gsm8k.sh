256c7704f3953fa7aaf27a0ca0732d8f574fa345
Yash More <yash.more@cerebras.net>
2025-10-01 13:32:55 -0400

    add gsm8k stuff

--- skyrl-train/examples/gsm8k/run_gsm8k.sh
+++ skyrl-train/examples/gsm8k/run_gsm8k.sh
@@ -3,13 +3,13 @@ set -x
 # Colocated GRPO training+generation for Qwen2.5-1.5B-Instruct on GSM8K.
 
 # uv run examples/gsm8k/gsm8k_dataset.py --output_dir $HOME/data/gsm8k
-# export WANDB_API_KEY=<your_key_here>
+export WANDB_API_KEY=<your_key_here>
 # bash examples/gsm8k/run_gsm8k.sh
 
 # NOTE (sumanthrh): `micro_train_batch_size_per_gpu` and `micro_forward_batch_size_per_gpu` can be tuned
 
 DATA_DIR="$HOME/data/gsm8k"
-NUM_GPUS=4
+NUM_GPUS=2
 LOGGER="wandb"  # change to "console" to print to stdout
 
 INFERENCE_BACKEND="vllm"
