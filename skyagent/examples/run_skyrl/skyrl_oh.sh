2db8f5948f8ca2eb3988591aa60d67f614b13fe9
Yash More <yash.more@cerebras.net>
2025-10-03 13:10:30 -0400

    remove kl traget

--- skyagent/examples/run_skyrl/skyrl_oh.sh
+++ skyagent/examples/run_skyrl/skyrl_oh.sh
@@ -34,7 +34,6 @@ uv run --isolated --extra skyrl-train --directory . --frozen --env-file .env -m 
   trainer.policy_mini_batch_size=4 \
   trainer.algorithm.use_kl_loss=false \
   trainer.algorithm.kl_loss_coef=0 \
-  trainer.algorithm.kl_target=null \
   trainer.algorithm.init_kl_coef=0.0 \
   trainer.algorithm.policy_loss_type="dual_clip" \
   trainer.algorithm.eps_clip_high=0.28 \
@@ -59,5 +58,4 @@ uv run --isolated --extra skyrl-train --directory . --frozen --env-file .env -m 
   environment.env_class=null \
   trainer.logger="wandb" \
   trainer.project_name="skyagent-32b-r2e" \
-  trainer.algorithm.kl_target=null \
   trainer.run_name="skyagent-32b-r2e-1000-acc-thinking-mini4"
